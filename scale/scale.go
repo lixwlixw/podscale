e7e9e586cee0d1ea5c4dc138a3aadc8abda88346
lixwlixw <871810908@qq.com>
2018-09-25 14:12:06 +0800

    Update scale.go

--- scale/scale.go
+++ scale/scale.go
@@ -11,17 +11,6 @@ import (
 )
 var apiHost string
 
-func getenv(env string) string {
-	env_value := os.Getenv(env)
-	if env_value == "" {
-		fmt.Println("FATAL: NEED ENV", env)
-		fmt.Println("Exit...........")
-		os.Exit(2)
-	}
-	fmt.Println("ENV:", env, env_value)
-	return env_value
-}
-
 func GenRequest(method, url, token string, body []byte) (*http.Response, error) {
  var req *http.Request
  var err error
