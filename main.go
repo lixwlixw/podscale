a0eaf0d387f9f001087d1a5617a8f0016bbe2a89
Daniel <18810155806@163.com>
2018-09-28 18:40:48 +0800

    add auth

--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
 
 import (
-	"github.com/lixwlixw/podscale/scale"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lixwlixw/podscale/scale"
 )
+
+var secrets = gin.H{
+	"test": gin.H{"email": "foo@gaojihealth.com", "phone": "123433"},
+}
 
 func main() {
 	router := handle()
 	s := &http.Server{
-		Addr:    ":10012",
-		Handler: router,
+		Addr:           ":10012",
+		Handler:        router,
 		WriteTimeout:   30 * time.Second,
 		MaxHeaderBytes: 0,
 	}
 	s.ListenAndServe()
 }
+func getSecrets(c *gin.Context) {
+	user := c.MustGet(gin.AuthUserKey).(string)
+	if _, ok := secrets[user]; ok {
+		c.JSON(200, "200")
+	} else {
+		c.JSON(200, "401")
+	}
+}
 
 func handle() (router *gin.Engine) {
 	gin.SetMode(gin.ReleaseMode)
 	router = gin.Default()
-        router.GET("/namespaces/:namespace/deployments/:name/scale", scale.ListReplicas)
+	authorized := router.Group("/namespaces", gin.BasicAuth(gin.Accounts{
+		"test": "test",
+	}))
+	authorized.GET("/:namespace/deployments/:name/scale", scale.ListReplicas, getSecrets)
 	router.PATCH("/namespaces/:namespace/deployments/:name/scale", scale.ScaleReplicas)
 	return
 }
